1ee568dcbfa8e78895de869d4392d1ea5d1de55c
Yusuf Sugiono C3322970 <c3322970@bangkit.academy>
2021-06-08 12:14:05 +0700

    change port number

--- covid-forecasting-api/src/server.js
+++ covid-forecasting-api/src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 80,
+    port: 5000,
     host: 'localhost',
     routes: {
       cors: {
